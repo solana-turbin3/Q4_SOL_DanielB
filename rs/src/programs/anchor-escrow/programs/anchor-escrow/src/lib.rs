8a382130d6a744fc620dc713b649e9475572d4db
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2024-10-25 12:00:12 +0200

    escrow with code from Shrinath - testing

--- rs/src/programs/anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ rs/src/programs/anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,20 +1,28 @@
+pub mod context;
+pub mod state;
+
 use anchor_lang::prelude::*;
 
 declare_id!("4W2xEwJ7K4Detthv2Qc6fZRV9rjM7RxfnKXVR5okaP9S");
 
-pub mod state;
-pub use sate::*;
-
-pub mod instruction;
-pub use instruction::*;
+pub use context::*;
+pub use state::*;
 
 #[program]
 pub mod anchor_escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Make>, seed:u64, receive:u64 ) -> Result<()> {
-        ctx.accoutns.init_escrow(seed, receive, &ctx.bumps)?;
+    
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
         ctx.accounts.deposit(deposit)?;
-        Ok(())
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
--- rs/src/programs/anchor-escrow/programs/anchor-escrow/src/state/escrow.rs
+++ rs/src/programs/anchor-escrow/programs/anchor-escrow/src/state/escrow.rs
@@ -2,11 +2,11 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct Escrow{
-    pub seed : u64,
+pub struct Escrow {
+    pub seed: u64,
     pub maker: Pubkey,
     pub mint_a: Pubkey,
     pub mint_b: Pubkey,
-    pub receive: u64,
+    pub recieve: u64,
     pub bump: u8
 }
