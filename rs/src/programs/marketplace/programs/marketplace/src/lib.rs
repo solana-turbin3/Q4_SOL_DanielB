ddb20225834ef06fbae4b8fcc17ffd614e573aa9
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2024-11-11 07:56:06 +0100

    adding market place

---
+++ rs/src/programs/marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+mod state;
+mod instructions;
+mod errors;
+
+declare_id!("5mNGM7AGzsEQCobetKetuhp6FJ9mZwGNor44PWPnD6g7");
+
+use instructions::*;
+use errors::*;
+
+#[program]
+pub mod anchor_marketplace {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+
+        Ok(())
+    }
+
+    pub fn listing(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+
+        Ok(())
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()?;
+
+        Ok(())
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()?;
+
+        Ok(())
+    }
+}
